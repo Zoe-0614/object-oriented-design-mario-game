e262e3f85a62b064e8d4073d992494699eb5b628
Zoe-0614 <zoelowpeiee@gmail.com>
2022-04-07 01:46:29 +0800

    Create Wrench class

---
+++ src/game/Wrench.java
@@ -0,0 +1,11 @@
+package game;
+
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+
+public class Wrench extends WeaponItem {
+
+    public Wrench() {
+        super("wrench", 'w', 50, "destroys", 80);
+    }
+
+}
