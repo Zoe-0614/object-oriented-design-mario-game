e1ffb9604dc9ef6b95422d24d615e7dcc8c5b19c
Zoe-0614 <zoelowpeiee@gmail.com>
2022-04-07 01:48:33 +0800

    Updated Floor class

--- src/game/Floor.java
+++ src/game/Floor.java
@@ -1,5 +1,6 @@
 package game;
 
+import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
 
 /**
@@ -8,5 +9,14 @@ import edu.monash.fit2099.engine.positions.Ground;
 public class Floor extends Ground {
 	public Floor() {
 		super('_');
+	}
+
+	@Override
+	public boolean canActorEnter(Actor actor) {
+		char isEnemies = actor.getDisplayChar();
+		if (isEnemies == 'K' || isEnemies == 'g') {
+			return false;
+		}
+		return true;
 	}
 }
