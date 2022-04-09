3fb967f9a89443aebdac84200d1777b4dcdbe9b5
Zoe-0614 <zoelowpeiee@gmail.com>
2022-04-09 16:51:35 +0800

    Add resetInstance in Tree class

---
+++ src/game/grounds/Tree.java
@@ -0,0 +1,56 @@
+package game.grounds;
+
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.reset.Resettable;
+
+import java.util.Random;
+
+abstract public class Tree extends Ground implements Resettable {
+    protected int age;
+
+    /**
+     * Constructor.
+     */
+    public Tree(char displayChar, int age) {
+        super(displayChar);
+        this.age = (age);
+        registerInstance();
+    }
+
+    @Override
+    public boolean canActorEnter(Actor actor) {
+        return false;
+    }
+
+    //For the trees to grow
+    public void tick(Location location) {
+        super.tick(location);
+        age++;
+//        System.out.println("age:"+age);
+        if (age == 10) {
+            location.setGround(new Sapling());
+
+        } else if (age == 20) {
+            location.setGround(new Mature());
+//            System.out.println("Successfully set ground to Mature");
+        }
+        //After every tick, drop goomba, koopa etc
+        drop(location);
+
+
+    }
+
+    @Override
+    public void resetInstance() {
+        int rand = new Random().nextInt(2);
+        if(rand == 0){
+
+        }
+    }
+
+    protected abstract void drop(Location location);
+
+}
