18d70b71304136d0419083b77520416b26779955
Chua Shin Herh <chuashinherh@gmail.com>
2022-04-30 19:14:54 +0800

    Added JavaDoc for REQ2

--- src/game/actions/JumpAction.java
+++ src/game/actions/JumpAction.java
@@ -7,17 +7,35 @@ import edu.monash.fit2099.engine.positions.Location;
 import game.capabilities.JumpCapable;
 import game.enums.Status;
 
+/**
+ * JumpAction is a class that handles the jumping actions of actors to high grounds.
+ *
+ * @author Chua Shin Herh
+ * @version 1.0
+ */
 public class JumpAction extends Action {
-    JumpCapable jumpCapableGround;
-    Location locationToJump;
-    String direction;
+    private JumpCapable jumpCapableGround;
+    private Location locationToJump;
+    private String direction;
 
+    /**
+     * A constructor for the JumpAction class.
+     * @param jumpCapableGround The ground that can be jumped to.
+     * @param location The location to jump to.
+     * @param direction The direction of the location to jump from the actor.
+     */
     public JumpAction(JumpCapable jumpCapableGround, Location location, String direction) {
         this.jumpCapableGround = jumpCapableGround;
         this.locationToJump = location;
         this.direction = direction;
     }
 
+    /**
+     * Perform the Jump Action.
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return a success message if the actor successfully jumped, a fail message if the actor failed the jump.
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         boolean isSuccess;
@@ -35,6 +53,11 @@ public class JumpAction extends Action {
         }
     }
 
+    /**
+     * Returns a string to show in the menu for the Jump Action.
+     * @param actor The actor performing the action.
+     * @return the text we put on the menu.
+     */
     @Override
     public String menuDescription(Actor actor) {
         return actor + " jumps to the " + direction + " " + jumpCapableGround.getName() +
--- src/game/capabilities/JumpCapable.java
+++ src/game/capabilities/JumpCapable.java
@@ -6,7 +6,20 @@ import edu.monash.fit2099.engine.positions.Location;
 
 import java.util.Random;
 
+/**
+ * JumpCapable is an interface where class that can be jumped to can implement.
+ *
+ * @author Chua Shin Herh
+ * @version 1.0
+ */
 public interface JumpCapable {
+    /**
+     * A default method for all Jump Capable classes to use which performs the actual jump action.
+     * @param actor The actor performing the jump.
+     * @param locationToJump The location to jump to.
+     * @param map The map of the game.
+     * @return a boolean indicating whether the jump has succeeded.
+     */
     default boolean jump(Actor actor, Location locationToJump, GameMap map) {
         int jumpChance = new Random().nextInt(100);
         if (jumpChance < getChance()) {
@@ -18,7 +31,21 @@ public interface JumpCapable {
         }
     }
 
+    /**
+     * Returns the name of the Jump Capable ground.
+     * @return a String which is the name of the Jump Capable ground.
+     */
     String getName();
+
+    /**
+     * Returns the damage dealt by the Jump Capable ground if the actor fails the jump.
+     * @return an integer which is the damage dealt by the Jump Capable ground if the actor fails the jump.
+     */
     int getDamage();
+
+    /**
+     * Returns the probability of a successful jump.
+     * @return an integer which is the probability of a successful jump.
+     */
     int getChance();
 }
