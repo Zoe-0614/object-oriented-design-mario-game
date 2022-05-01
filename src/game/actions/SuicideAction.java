43f003edce5108bfc687a90852f4038c4b09e8d0
Zoe-0614 <zoelowpeiee@gmail.com>
2022-05-01 23:18:04 +0800

    Fix Suicide Action and Consume Action

--- src/game/actions/SuicideAction.java
+++ src/game/actions/SuicideAction.java
@@ -12,16 +12,10 @@ public class SuicideAction extends Action {
     /**
      * The actor performing suicide gets removed from the map.
      *
-     * @param target The actor performing the action.
+     * @param actor The actor performing the action.
      * @param map The map the actor is on.
-     * @param location The location the actor is on.
      * @return a suitable description to display in the UI
      */
-//    public String execute(Actor target, GameMap map, Location location) {
-//        map.removeActor(target);
-//        return target + "suicides... at" + location.toString();
-//    }
-
     @Override
     public String execute(Actor actor, GameMap map) {
         map.removeActor(actor);
