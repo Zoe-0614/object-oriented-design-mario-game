ded24d4b737fcb61f860ef57b26a503539dc9a26
Zoe-0614 <zoelowpeiee@gmail.com>
2022-05-02 20:10:14 +0800

    Update package-info.java

--- src/game/package-info.java
+++ src/game/package-info.java
@@ -1,5 +1,5 @@
 /**
- * A text-based “rogue-like” game. Rogue-like games are named after the first such program: a fantasy game named Rogue. They were very popular in the days before home computers were capable of elaborate graphics and still have a small but dedicated following.
+ * A text-based rogue-like game. Rogue-like games are named after the first such program: a fantasy game named Rogue. They were very popular in the days before home computers were capable of elaborate graphics and still have a small but dedicated following.
  * This package includes the Application class, Player, Toad and Wallet.
  * @since 1.0
  * @see java.awt
