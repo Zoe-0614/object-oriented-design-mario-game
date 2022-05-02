a5d60dd7cfc4c755a7c2edfd9d5f88ddc9891d02
Loh Zhun Guan <zloh0009@student.monash.edu>
2022-05-02 21:06:48 +0800

    Javadoc SpawnCapable

--- src/game/capabilities/SpawnCapable.java
+++ src/game/capabilities/SpawnCapable.java
@@ -2,6 +2,13 @@ package game.capabilities;
 
 import edu.monash.fit2099.engine.positions.Location;
 
+/**
+ * SpawnCapable is an interface where classes that can spawn things can implement
+ */
 public interface SpawnCapable {
+    /**
+     * Method that spawns things at their current location
+     * @param location
+     */
     void drop(Location location);
 }
