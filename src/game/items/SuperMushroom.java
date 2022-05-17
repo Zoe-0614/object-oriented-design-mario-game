c7385707aca13bdc1988968c232dd2b89aec27c6
Zoe-0614 <zoelowpeiee@gmail.com>
2022-05-17 16:38:37 +0800

    Remove unwanted classes

--- src/game/items/SuperMushroom.java
+++ src/game/items/SuperMushroom.java
@@ -4,7 +4,6 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
 import game.actions.ConsumeAction;
-import game.actions.ConsumeSuperMushroomAction;
 import game.capabilities.Buyable;
 import game.enums.Status;
 
